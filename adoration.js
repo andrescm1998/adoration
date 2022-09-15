4daecd755211ea106d0341bb21ff52f9b69d3b7c
Andres Carrasco <andrescm1998@outlook.com>
2022-09-15 09:55:50 +0100

    Update adoration.js

--- adoration.js
+++ adoration.js
@@ -1,6 +1,6 @@
 function showHowMuchILoveYou (amount){
     if (typeof amount == 'number' && Number.isInteger(amount) && amount > 0){
-        return `I love you th${'i'.repeat(amount)}s much`;
+        return `I love you th${'0'.repeat(amount)}s much`;
     } else {
         throw new Error("Not a valid amount mate")
     }
